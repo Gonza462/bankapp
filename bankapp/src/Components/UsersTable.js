f3e4dc3a4549209cecdf621b37eb73fb1631fde8
Luis Gonzalez <gonza462@uwm.edu>
2021-01-14 14:42:48 -0600

    deleted some stuff

--- bankapp/src/Components/UsersTable.js
+++ bankapp/src/Components/UsersTable.js
@@ -92,13 +92,7 @@ function Row(props) {
     <React.Fragment>
       <TableRow className={classes.root}>
         <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
+        
         </TableCell>
         <TableCell component="th" scope="row">
           {row.idUser}
@@ -131,40 +125,7 @@ function Row(props) {
        
       </TableRow>
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                Transaction History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Transaction ID</TableCell>
-                    <TableCell>Account ID</TableCell>
-                    <TableCell>Amount</TableCell>
-                  
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {[].map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
+       
       </TableRow>
     </React.Fragment>
   );
